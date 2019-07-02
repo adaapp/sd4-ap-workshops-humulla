5e0683cc935cadb3d669ad9a39562fc71190d743
Humairaa Mulla <humairaa.mulla@hackney.gov.uk>
2019-07-02 15:25:05 +0100

    Ada farm

--- example-ada-farm/models/Crop.js
+++ example-ada-farm/models/Crop.js
@@ -1,16 +1,16 @@
 class Crop {
     constructor(type, field, x, y) {
-        this.type = type
+        this._type = type
         // Get the image based on the name of the field
-        this.image = loadImage('/images/'+type.toLowerCase()+'.png') // ./carrot.png
-        this.health = 0
-        this.field = field
-        this.x = x
-        this.y = y
-        this.birthstamp = millis() / 1000
-        this.width = 1
-        this.height = 1
-        this.healthySize = 30
+        this._image = loadImage('/images/'+type.toLowerCase()+'.png') // ./carrot.png
+        this._health = 0
+        this._field = field
+        this._x = x
+        this._y = y
+        this._birthstamp = millis() / 1000
+        this._width = 1
+        this._height = 1
+        this._healthySize = 30
 
         // Draw the crop when it's created
         this.draw()
@@ -18,23 +18,23 @@ class Crop {
 
     getAge() {
         // return the amount of time it's been alive
-        return millis() / 1000 - this.birthstamp
+        return millis() / 1000 - this._birthstamp
     }
 
     draw() {
         // set a width for the image based on its age
-        if (this.getAge() < this.healthySize ) {
-            var width = this.width * this.getAge()
-            var height = this.height * this.getAge()
+        if (this.getAge() < this._healthySize ) {
+            var width = this._width * this.getAge()
+            var height = this._height * this.getAge()
         } else {
             var width = 30;
             var height = 30;
         }
  
         image(
-            this.image,
-            this.x,
-            this.y,
+            this._image,
+            this._x,
+            this._y,
             width,
             height
         )
